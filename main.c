027924ed16ff61af60f86249bfdba04d3c20d392
huang666 <yuhaoh500@gmail.com>
2026-09-21 21:18:57 +0800

    已经在feature分支上做了一些修改

--- main.c
+++ main.c
@@ -6,5 +6,7 @@ int main()
     printf("Hello, world!\n");
     // 做一点有趣的事情
     printf("Let's do something fun!\n");
+    // 在feature分支上添加一些有趣的代码
+    printf("This is a fun feature!\n");
     return 0;
 }
