05bdf74a1ccbddd019117381765380897b082f65
huang666 <yuhaoh500@gmail.com>
2026-09-21 21:23:23 +0800

    提交

--- main.c
+++ main.c
@@ -8,5 +8,7 @@ int main()
     printf("Let's do something fun!\n");
     // 在feature分支上添加一些有趣的代码
     printf("This is a fun feature!\n");
+    // 刚刚没看清楚，在feature分支上添加一些有趣的代码，随后在main分支上添加一些有趣的代码并处理冲突
+    printf("This is a fun feature on the main\n");
     return 0;
 }
